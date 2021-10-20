a04570c1cf347a82bdf9cf5807a30c5f1ac0205c
Felipe Reis <contacto.felipereis@gmail.com>
2021-10-20 15:58:16 +0100

    test husky

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  const bla = 'foo';
+  const bla = 'fooo';
 
   return (
     <div className={styles.container}>
