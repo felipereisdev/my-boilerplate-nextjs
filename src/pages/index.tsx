a8c93c3d97f58c8476cb2614cb72c9b79c8c1660
Felipe Reis <contacto.felipereis@gmail.com>
2021-10-20 15:49:44 +0100

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  const bla = 'fo';
+  const bla = 'foo';
 
   return (
     <div className={styles.container}>
