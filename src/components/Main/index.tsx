dcef2500b9d5b4988a0aaea60f5b99ac63fbc1d8
Felipe Reis <contacto.felipereis@gmail.com>
2021-10-22 15:09:36 +0100

    finished boilerplate

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,21 @@
-export function Main() {
+import { Wrapper, Title, Description, Logo, Illustration } from './styles';
+
+export function Main({
+  title = 'React Avançado',
+  description = 'React, Typescript, Next.js, Styled Components and Jest'
+}) {
   return (
-    <main>
-      <h1>React Avançado</h1>
-    </main>
+    <Wrapper>
+      <Logo
+        src="/img/logo.svg"
+        alt="Imagem de um átomo e react avançado escrito ao lado"
+      />
+      <Title>{title}</Title>
+      <Description>{description}</Description>
+      <Illustration
+        src="/img/hero-illustration.svg"
+        alt="Um desenvolvedor sentado de frente para uma tela com código"
+      />
+    </Wrapper>
   );
 }
